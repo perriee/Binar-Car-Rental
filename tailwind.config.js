2e31cd0c53e9825db2cd4915093c68ea528f8ed7
Ferry Febriansyah <perriebenhard@gmail.com>
2023-08-26 11:03:04 +0700

    fix: change content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  content: ["./build/*.html", "./build/js/*.js", './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
