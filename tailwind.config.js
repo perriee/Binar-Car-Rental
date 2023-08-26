5f214bb73ad2e360eb6a3e5b8f5a98c7e87056bf
Ferry Febriansyah <perriebenhard@gmail.com>
2023-08-26 10:19:53 +0700

    refactor: restructure folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./**/*.{html,js}', './node_modules/flowbite/**/*.js'],
+  content: ['./build/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {},
   },
